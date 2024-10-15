02ac6a355e6b061a57f27f778bdb937a7dfa439e
LeeYooBin <santosroberdan25@gmail.com>
2024-10-15 12:14:32 -0300

    feat: add MachinesModule to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { CommandsModule } from './commands/commands.module';
 import { PrismaModule } from '../prisma/prisma.module';
+import { MachinesModule } from './machines/machines.module';
 
 @Module({
-  imports: [AuthModule, CommandsModule, PrismaModule],
+  imports: [AuthModule, PrismaModule, MachinesModule, CommandsModule],
   providers: [],
 })
 export class AppModule {}
