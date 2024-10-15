51b9fd76cf4c01b9a36f3aee3bc3794530d21b4f
LeeYooBin <santosroberdan25@gmail.com>
2024-10-15 12:16:01 -0300

    feat: create MachineController with endpoints to get all machines and a machine by id

---
+++ src/machines/controller/machine.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { MachineService } from '../service/machine.service';
+import { agent } from '@prisma/client';
+
+@Controller('machines')
+export class MachineController {
+  constructor(private readonly machineService: MachineService) {}
+
+  @Get()
+  async getMachines(): Promise<agent[]> {
+    return this.machineService.getAllMachines();
+  }
+
+  @Get("/:id")
+  async getMachine(@Param('id') id: string): Promise<agent> {
+    return this.machineService.getMachineById(id);
+  }
+}
