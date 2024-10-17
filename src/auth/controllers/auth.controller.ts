5c31de0b6db79cff28372f09b042972b768c6a44
LeeYooBin <santosroberdan25@gmail.com>
2024-10-17 09:40:55 -0300

    doc: adding documentation of auth endpoints

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,22 +1,50 @@
 import { Controller, Post, Body, UseGuards } from '@nestjs/common';
 import { AuthService } from '../services/auth.service';
 import { JwtAuthGuard } from '../jwt/jwt.auth.guard';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 
-@ApiTags()
-@Controller('auth')
+@ApiTags('Auth')
 @ApiBearerAuth()
+@Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
-  async login(@Body('username') username: string, @Body('password') password: string) {
+  @ApiOperation({ summary: 'Login de usuário' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', description: 'Nome de usuário' },
+        password: { type: 'string', description: 'Senha do usuário' },
+      },
+      required: ['username', 'password'],
+    },
+  })
+  async login(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
     return this.authService.authenticateUser(username, password);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post('register')
-  async register(@Body('username') username: string, @Body('password') password: string) {
+  @ApiOperation({ summary: 'Registrar um novo usuário' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        username: { type: 'string', description: 'Nome de usuário' },
+        password: { type: 'string', description: 'Senha do usuário' },
+      },
+      required: ['username', 'password'],
+    },
+  })
+  async register(
+    @Body('username') username: string,
+    @Body('password') password: string,
+  ) {
     return this.authService.createUser(username, password);
   }
-}     
+}
