9f6a0c93899770fd5ca3a1cf7df8b9dec046a848
LeeYooBin <santosroberdan25@gmail.com>
2024-10-17 07:44:34 -0300

    fix: adjusting command controller

--- src/commands/controllers/commands.controller.ts
+++ src/commands/controllers/commands.controller.ts
@@ -1,26 +1,23 @@
-import { Body, Controller, Get, Post, UseGuards } from "@nestjs/common";
-import { CommandsService } from "../services/commands.service";
-import { JwtAuthGuard } from "src/auth/jwt/jwt.auth.guard";
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
+import { CommandsService } from '../services/commands.service';
+import { JwtAuthGuard } from 'src/auth/jwt/jwt.auth.guard';
 
-@Controller("command")
-export class CommandsController{
+@Controller()
+export class CommandsController {
+  constructor(private readonly commandService: CommandsService) {}
 
-    constructor(private readonly commandsQueued: CommandsService){}
+  @UseGuards(JwtAuthGuard)
+  @Post('send-command')
+  async addCommand(
+    @Body('machine_id') machineId: string,
+    @Body('action') action: string,
+  ) {
+    return await this.commandService.addCommand(action, machineId);
+  }
 
-    @UseGuards(JwtAuthGuard)
-    @Post("add-command")
-    async addCommand(
-    @Body("action") action: string, 
-    @Body("params") params: any,
-    @Body("machineId") machineId: string
-){
-        const newCommand = await this.commandsQueued.addCommand(action, params, machineId);
-        return newCommand; 
-    }
-
-    @UseGuards(JwtAuthGuard)
-    @Get("next-response")
-    async getNextResponse(){
-        return await this.commandsQueued.getResponseCommand();
-    }
+//   @UseGuards(JwtAuthGuard)
+  // @Get('get-response')
+  // async getNextResponse() {
+  //   return await this.commandService.getResponseCommand();
+  // }
 }
